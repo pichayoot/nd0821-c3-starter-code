5b791f82fe50a6c37dfc181156966cb8713233d7
Pichayoot Ouppaphan <pichayoot@gmail.com>
2024-10-21 12:49:39 -0400

    print encoded_features shape for debugging

--- starter/main.py
+++ starter/main.py
@@ -78,6 +78,7 @@ def read_item(item: Item):
     raw_features.columns = [x.replace('_', '-') for x in raw_features.columns]
     encoded_features, _, _, _ = process_data(raw_features, cat_features, training=False, encoder=encoder, lb=lb)
     print(encoded_features)
+    print(encoded_features.shape)
     pred = lb.inverse_transform(inference(model, encoded_features))[0]
     output = {'output': str(pred)}
     return output
